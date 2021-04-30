5f13c626fbae7193e385dba053d9644291faa7d5
Johnny Magrippis <j@magrippis.com>
2021-04-30 19:34:35 +0100

    ♻️ early access benefits in list form

--- src/components/Home/EarlyAccess/EarlyAccess.tsx
+++ src/components/Home/EarlyAccess/EarlyAccess.tsx
@@ -12,10 +12,16 @@ const EarlyAccess = () => {
 		<section className="container">
 			<div className="px-4 lg:px-0">
 				<h2 className="text-2xl mb-4">Get early access to our episodes!</h2>
-				<p className="text-lg text-gray-400 mb-4">
-					See our episodes first, while they‘re still unlisted, and get those
-					early comments in!
-				</p>
+				<ul className="text-lg text-gray-400 mb-4 list-disc list-inside">
+					<li>
+						Be the first to watch our episodes, while they‘re still unlisted
+					</li>
+					<li>Get your comments and feedback in, before everyone else</li>
+					<li>
+						Read behind-the-scenes scoops, straight from Marily & Johnny, to
+						your inbox
+					</li>
+				</ul>
 			</div>
 			<div ref={containerRef} />
 		</section>
