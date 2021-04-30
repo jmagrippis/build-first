3496bd5cf6521c0c7c672be78236cdc951947c36
Johnny Magrippis <j@magrippis.com>
2021-04-30 15:49:16 +0100

    ✨ get latest post through the Airtable API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,18 @@
 import {GetStaticProps} from 'next'
 
 import Home, {HomeProps} from 'components/Home'
+import {episodesRepo} from 'lib/repos/episodes/Airtable'
 import {YOUTUBE_CHANNEL_URI} from 'lib/constants'
 
-export const getStaticProps: GetStaticProps<HomeProps> = async () => ({
-	props: {
-		latestEpisodeLink: YOUTUBE_CHANNEL_URI,
-	},
-	revalidate: 60,
-})
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+	const latestEpisode = await episodesRepo.latestEpisode().catch()
+
+	return {
+		props: {
+			latestEpisodeLink: latestEpisode?.youTubeLink || YOUTUBE_CHANNEL_URI,
+		},
+		revalidate: 60,
+	}
+}
 
 export default Home
