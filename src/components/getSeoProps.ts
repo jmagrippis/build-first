491d97c890d242a52a1ce124d5cbb430482bd421
Johnny Magrippis <j@magrippis.com>
2021-04-30 08:11:04 +0100

    🔍  custom favicon

--- src/components/getSeoProps.ts
+++ src/components/getSeoProps.ts
@@ -3,7 +3,7 @@ import {rootUrl} from 'lib/constants'
 const defaultSeo = {
 	title: 'Build First | The fun way to learn tech!',
 	description:
-		'Marily Nika and Johnny Magrippis are tech-industry veterans here to help you grow into tech!',
+		'Dr. Marily Nika and Johnny Magrippis are tech-industry veterans, here to help you grow into tech! We’ll build stuff with you, and for you 🙌',
 	openGraph: {
 		type: 'website',
 		locale: 'en_UK',
@@ -11,10 +11,10 @@ const defaultSeo = {
 		site_name: 'Build First',
 		images: [
 			{
-				url: `${rootUrl}/images/hero.jpg`,
+				url: `${rootUrl}/images/hero.png`,
 				width: 512,
 				height: 512,
-				alt: 'Johnny Magrippis',
+				alt: 'Build First',
 			},
 		],
 	},
