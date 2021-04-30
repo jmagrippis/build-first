17239dee7cef937f05257fbb4aec4f5e239f0344
Johnny Magrippis <j@magrippis.com>
2021-04-30 15:49:41 +0100

    🔥  do not track custom plausible events

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,22 +1,5 @@
 import Document, {Html, Head, Main, NextScript} from 'next/document'
 
-export enum TrackEvent {
-	SignupIframeEnter = 'signup_iframe_enter',
-}
-
-type PlausibleArgs = [TrackEvent, () => void] | [TrackEvent]
-
-declare global {
-	const plausible: {
-		(...args: PlausibleArgs): void
-		q?: PlausibleArgs[]
-	}
-
-	interface Window {
-		plausible?: typeof plausible
-	}
-}
-
 // extending classes in Javascript?! The official suggestion from the Next.js team:
 // https://github.com/zeit/next.js/blob/canary/examples/with-google-analytics/pages/_document.js
 class CustomDocument extends Document {
@@ -24,6 +7,7 @@ class CustomDocument extends Document {
 		return (
 			<Html>
 				<Head>
+					{/* favicons */}
 					<link rel="icon" href="/favicon.ico" />
 					<link
 						rel="apple-touch-icon"
@@ -43,22 +27,18 @@ class CustomDocument extends Document {
 						href="/favicon-16x16.png"
 					/>
 					<link rel="manifest" href="/site.webmanifest" />
+					{/* fonts */}
 					<link rel="preconnect" href="https://fonts.gstatic.com" />
 					<link
 						href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300&family=New+Tegomin&display=swap"
 						rel="stylesheet"
 					/>
+					{/* analytics */}
 					<script
 						async
 						defer
 						data-domain="buildfirst.tech"
 						src="https://plausible.buildfirst.tech/js/index.js"
-					/>
-					<script
-						dangerouslySetInnerHTML={{
-							__html:
-								'window.plausible = window.plausible || function() { (window.plausible.q = window.plausible.q || []).push(arguments) }',
-						}}
 					/>
 				</Head>
 				<body>
