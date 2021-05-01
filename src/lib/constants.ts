aaf54a6c3e24b40ac725d2ac177466fd3beafcb1
Johnny Magrippis <j@magrippis.com>
2021-05-01 10:33:03 +0100

    🐛  get the host using the new-style vercel env vars

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,8 @@
 const protocol = process.env.NODE_ENV === 'production' ? 'https' : 'http'
+const host =
+	process.env.NEXT_PUBLIC_ROOT_URL || process.env.NEXT_PUBLIC_VERCEL_URL
 
-export const rootUrl = `${protocol}://${process.env.NEXT_PUBLIC_ROOT_URL}`
+export const rootUrl = `${protocol}://${host}`
 
 export const YOUTUBE_CHANNEL_URI =
 	'https://www.youtube.com/channel/UCvw69Hm5o2150tmgzSdNkyg'
